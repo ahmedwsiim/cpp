be22776d0149d313176bf04b571f5df1f67638fd
Muhammad Ahmad <76181595+ahmedwsiim@users.noreply.github.com>
2022-09-28 23:31:47 +0500

    Add files via upload

---
+++ c=a+b.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main ()
+{
+	int a;
+	int b;
+	int c;
+	cout<<"Bhai a ki value do"<<endl;
+	cin>> a;
+	cout<<"b ki bhi de do mehrbani hogi"<<endl;
+	cin>> b;
+	c=a+b;
+	cout<<"Apka matlooba jawab hazir hai sarkar="<<c<<endl;
+	getch();
+	return 0;
+}
